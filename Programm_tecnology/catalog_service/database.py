4cb28e6860de61e3e8373a55f0e46a663e2b63d2
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-05-30 01:29:02 +1000

    Update database.py

--- Programm_tecnology/catalog_service/database.py
+++ Programm_tecnology/catalog_service/database.py
@@ -1,1 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
+# Берем URL базы из переменных окружения (для Docker)
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@db:5432/catalog_db")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
