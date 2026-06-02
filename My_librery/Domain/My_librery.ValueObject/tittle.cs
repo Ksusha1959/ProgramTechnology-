23025924d92a200c7524318c94181796198b98d7
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:09:44 +1000

    Update tittle.cs

--- My_librery/Domain/My_librery.ValueObject/tittle.cs
+++ My_librery/Domain/My_librery.ValueObject/tittle.cs
@@ -1,1 +1,7 @@
+using NotesService.ValueObjects.Base;
+using NotesService.ValueObjects.Validators;
 
+namespace NotesService.ValueObjects;
+
+
+public class Tittle(string name) : ValueObject<string>(new TittleValidator(), name);
