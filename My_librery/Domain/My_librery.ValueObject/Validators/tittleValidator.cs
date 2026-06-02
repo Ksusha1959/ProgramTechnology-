dab4e09344778bea400d32b40e0fd8c6051ce230
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:34:04 +1000

    Update tittleValidator.cs

--- My_librery/Domain/My_librery.ValueObject/Validators/tittleValidator.cs
+++ My_librery/Domain/My_librery.ValueObject/Validators/tittleValidator.cs
@@ -1,1 +1,21 @@
+using My_librery.ValueObjects.Base;
+using My_librery.ValueObjects.Exceptions;
 
+namespace My_librery.ValueObjects.Validators;
+
+public class titleValidator : IValidator<string>
+{
+
+    
+    public static int MAX_LENGTH => 50;
+
+  
+    public void Validate(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentNullOrWhiteSpaceException(nameof(value));
+
+        if (value.Length > MAX_LENGTH)
+            throw new ArgumentLongValueException(nameof(value), value, MAX_LENGTH);
+    }
+}
