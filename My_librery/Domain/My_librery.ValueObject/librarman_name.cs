28f70199703192d346795e06ccbfe8391d0fe333
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:19:52 +1000

    Update librarman_name.cs

--- My_librery/Domain/My_librery.ValueObject/librarman_name.cs
+++ My_librery/Domain/My_librery.ValueObject/librarman_name.cs
@@ -1,1 +1,10 @@
+using My_librery.ValueObjects.Base;
+using My_librery.ValueObjects.Validators;
 
+namespace My_librery.ValueObjects;
+
+/// <summary>
+/// Represents type of the entity's username.
+/// </summary>
+/// <param name="name">The username of the entity.</param>
+public class librarman_name(string name) : ValueObject<string>(new librarman_nameValidator(), name);
