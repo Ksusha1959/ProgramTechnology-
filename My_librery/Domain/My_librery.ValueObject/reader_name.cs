41e602f4f81bc585622213a322f1bfe71d4845fa
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:16:25 +1000

    Update reader_name.cs

--- My_librery/Domain/My_librery.ValueObject/reader_name.cs
+++ My_librery/Domain/My_librery.ValueObject/reader_name.cs
@@ -1,1 +1,6 @@
+using My_librery.ValueObjects.Base;
+using My_librery.ValueObjects.Validators;
 
+namespace My_librery.ValueObjects;
+
+public class reader_name(string name) : ValueObject<string>(new reader_nameValidator(), name);
