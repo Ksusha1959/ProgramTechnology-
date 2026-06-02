be15c7d382ee62a1b9144be3ae275493a736f7c5
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:14:52 +1000

    Update tittle.cs

--- My_librery/Domain/My_librery.ValueObject/tittle.cs
+++ My_librery/Domain/My_librery.ValueObject/tittle.cs
@@ -4,4 +4,4 @@ using My_librery.ValueObjects.Validators;
 namespace My_librery.ValueObjects;
 
 
-public class Tittle(string name) : ValueObject<string>(new TittleValidator(), name);
+public class tittle(string name) : ValueObject<string>(new tittleValidator(), name);
