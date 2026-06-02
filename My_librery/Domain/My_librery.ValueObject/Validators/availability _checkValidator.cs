b2cf47d9b8f43a84c904949b9b248fd66f8b2a92
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:28:50 +1000

    Update availability _checkValidator.cs

--- My_librery/Domain/My_librery.ValueObject/Validators/availability _checkValidator.cs
+++ My_librery/Domain/My_librery.ValueObject/Validators/availability _checkValidator.cs
@@ -1,1 +1,15 @@
+using My_librery.ValueObjects.Base;
+using My_librery.ValueObjects.Exceptions;
 
+namespace My_librery.ValueObjects.Validators;
+
+  
+    public class  : IValidator<string>
+{
+    
+    public void availability _checkValidator(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentNullOrWhiteSpaceException(nameof(value));
+    }
+}
