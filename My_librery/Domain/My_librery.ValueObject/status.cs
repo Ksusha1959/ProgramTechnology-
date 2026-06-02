cbcefd66cf8d9505818c33f5672d0e252cbccbe3
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:12:33 +1000

    Update status.cs

--- My_librery/Domain/My_librery.ValueObject/status.cs
+++ My_librery/Domain/My_librery.ValueObject/status.cs
@@ -1,1 +1,7 @@
+using My_librery.ValueObjects.Base;
+using My_librery.ValueObjects.Validators;
 
+namespace My_librery.ValueObjects;
+
+
+public class Status(string name) : ValueObject<string>(new StatusValidator(), name);
