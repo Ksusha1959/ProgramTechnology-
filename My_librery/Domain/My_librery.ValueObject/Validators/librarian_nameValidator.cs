59f0bdc643b740415f2b5e04e00252e183f3161d
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:32:05 +1000

    Update librarian_nameValidator.cs

--- My_librery/Domain/My_librery.ValueObject/Validators/librarian_nameValidator.cs
+++ My_librery/Domain/My_librery.ValueObject/Validators/librarian_nameValidator.cs
@@ -1,1 +1,15 @@
+using My_librery.ValueObjects.Base;
+using My_librery.ValueObjects.Exceptions;
 
+namespace My_librery.ValueObjects.Validators;
+
+   
+    public class librarian_nameValidator : IValidator<string>
+{
+    
+    public void Validate(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new ArgumentNullOrWhiteSpaceException(nameof(value));
+    }
+}
