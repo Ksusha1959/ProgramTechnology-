189e93e62aa1141f4a85703f5f46b2f3ec6cd806
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 00:22:04 +1000

    Update availability_check.cs

--- My_librery/Domain/My_librery.ValueObject/availability_check.cs
+++ My_librery/Domain/My_librery.ValueObject/availability_check.cs
@@ -4,4 +4,4 @@ using NotesService.ValueObjects.Validators;
 namespace NotesService.ValueObjects;
 
 
-public class availability_check(string name) : ValueObject<string>(new availability_check(), name);
+public class availability_check(string name) : ValueObject<string>(new availability_checkValidator(), name);
