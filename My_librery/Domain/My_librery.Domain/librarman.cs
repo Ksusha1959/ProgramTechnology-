f1b9075c95ee82085fc0165607120e3e42c19c3d
Ksusha1959 <kseniavtorusina499@gmail.com>
2026-06-03 20:04:30 +1000

    Update librarman.cs

--- My_librery/Domain/My_librery.Domain/librarman.cs
+++ My_librery/Domain/My_librery.Domain/librarman.cs
@@ -5,14 +5,14 @@ namespace My_library.Domain
 {
     public class Librarian : Entity<Guid>
     {
-        // Свойство Name соответствует полю 'name' (ФИО библиотекаря) из вашей схемы
+        
         public string Name { get; private set; } = default!;
 
-        // Конструктор для создания нового сотрудника
+        
         public Librarian(string name)
             : this(Guid.NewGuid(), name) { }
 
-        // Защищенный конструктор для восстановления сущности (например, из БД)
+        
         protected Librarian(Guid id, string name)
             : base(id)
         {
@@ -22,7 +22,7 @@ namespace My_library.Domain
             Name = name;
         }
 
-        // Метод для изменения ФИО (бизнес-логика)
+       
         public bool UpdateName(string newName)
         {
             if (string.IsNullOrWhiteSpace(newName))
